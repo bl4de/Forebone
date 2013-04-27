df317cf21b377c5bf17f8cfb7f7419b3a5bc7e4b
bl4de <bloorq@gmail.com>
2013-04-27 08:55:59 +0200

    Gruntfile.js and package.js; added tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,39 @@
 module.exports = function(grunt) {
     grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
         uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+            },
             dist: {
                 src: 'lib/forebone.js',
                 dest: 'dist/forebone.min.js'
-            },
+            }
+        },
+
+        jshint: {
+            files: ['Gruntfile.js', 'lib/**/*.js'],
+            options: {
+                force: true,
+                curly: true,
+                eqeqeq: true,
+                eqnull: true,
+                browser: true,
+                globals: {
+                    Forebone: true
+                }
+            }
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
 
-    grunt.registerTask('default', ['uglify']);
-}
+    // default - all tasks
+    grunt.registerTask('default', ['jshint', 'uglify']);
+    // test - jshint, testing
+    grunt.registerTask('test', ['jshint']);
+
+    // build - only dist
+    grunt.registerTask('build', ['uglify']);
+};
