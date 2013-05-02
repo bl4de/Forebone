f6b3de57814eb98fa5b1c1f33219026ac8cb97df
bl4de <bloorq@gmail.com>
2013-05-02 23:10:31 +0200

    unit tests (Qunit) added; base test suite; Grunt task

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,16 +23,22 @@ module.exports = function(grunt) {
                     Forebone: true
                 }
             }
+        },
+
+        qunit: {
+            files: ['test/**/*.html']
         }
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-qunit');
 
     // default - all tasks
-    grunt.registerTask('default', ['jshint', 'uglify']);
+    grunt.registerTask('default', ['jshint', 'qunit', 'uglify']);
+
     // test - jshint, testing
-    grunt.registerTask('test', ['jshint']);
+    grunt.registerTask('test', ['jshint', 'qunit']);
 
     // build - only dist
     grunt.registerTask('build', ['uglify']);
